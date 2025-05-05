c8d9023490efbc18f8dbb55d7080435e2191aac2
elizabeth koch <elk677@g.harvard.edu>
2025-05-05 18:17:57 -0400

    fix audio toggle

--- scripts/router.js
+++ scripts/router.js
@@ -19,12 +19,36 @@ function loadPage(hash) {
     });
 }
 
-window.addEventListener("hashchange", () => {
-  loadPage(window.location.hash);
+document.addEventListener("DOMContentLoaded", () => {
+  // Load the initial page
+  loadPage(window.location.hash || "#home");
+
+  // Set up audio toggle
+  const audio = document.getElementById("ambient-audio");
+  const toggle = document.getElementById("audio-toggle");
+
+  if (audio && toggle) {
+    audio.volume = 0.4;
+    audio.loop = true;
+    audio.play();
+    toggle.innerHTML = audio.paused
+      ? '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M8 5v14l11-7z"/></svg>'
+      : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>';
+
+    toggle.addEventListener("click", () => {
+      if (audio.paused) {
+        audio.play();
+        toggle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>';
+      } else {
+        audio.pause();
+        toggle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M8 5v14l11-7z"/></svg>';
+      }
+    });
+  }
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-  loadPage(window.location.hash || "#home");
+window.addEventListener("hashchange", () => {
+  loadPage(window.location.hash);
 });
 
 // Cycling through 3 placeholder images (placeholder1.png to placeholder3.png)
